d0291d3419e5c411d0e73fe094c4c41bd47c35b4
Refai <Mohamedref11@gmail.com>
2023-03-21 16:50:18 +0200

    Initial Commit

---
+++ CW4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CW4
+//
+//  Created by Mohamed El-Refai on 21/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
